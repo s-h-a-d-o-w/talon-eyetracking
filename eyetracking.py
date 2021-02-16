b2e1f00627b9af3ac09883778a129317590b6116
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2021-02-16 16:12:19 +0100

    Initial commit.

---
+++ eyetracking.py
@@ -0,0 +1,33 @@
+from talon import actions, app, Module
+from talon_plugins import eye_zoom_mouse
+
+def disable_eye_tracking():
+    # Menu point stays unticked, don't know how to tick it.
+    # talon.app.menu.toggle('Control Mouse (Zoom)', True) doesn't work and I don't know how to either provide or get the parent 'Eye Tracking'.
+    print("Disabling eye tracking (zoom mouse).")
+    eye_zoom_mouse.toggle_zoom_mouse(False)
+
+def enable_eye_tracking():
+    # Menu point stays unticked, don't know how to tick it.
+    # talon.app.menu.toggle('Control Mouse (Zoom)', True) doesn't work and I don't know how to either provide or get the parent 'Eye Tracking'.
+    print("Enabling eye tracking (zoom mouse).")
+    eye_zoom_mouse.toggle_zoom_mouse(True)
+
+
+mod = Module()
+@mod.action_class
+class Actions:
+    def eyes_on():
+        """Enables eye tracking (zoom mouse)."""
+        enable_eye_tracking()
+    def eyes_off():
+        """Disables eye tracking (zoom mouse)."""
+        disable_eye_tracking()
+
+
+def app_launch():
+    # print("Disabling SRE.")
+    # actions.speech.disable()
+    enable_eye_tracking()
+
+app.register("launch", app_launch)
---
+++ eyetracking.talon
@@ -0,0 +1,3 @@
+# Only e.g. "eyes on", "tracking on", "eye tracking zoom", "track my eyes" cause false positives.
+track my eyes zoom: user.eyes_on()
+stop tracking my eyes: user.eyes_off()
