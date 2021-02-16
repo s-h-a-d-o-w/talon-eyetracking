e0d4f8d20eabf1f34f659605505c6f2f43b0c3b3
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2021-02-16 20:30:24 +0100

    Add tracking calibration

--- eyetracking.py
+++ eyetracking.py
@@ -1,5 +1,12 @@
 from talon import actions, app, Module
 from talon_plugins import eye_zoom_mouse
+from talon_plugins import eye_mouse
+
+def calibrate_eye_tracking():
+    # Menu point stays unticked, don't know how to tick it.
+    # talon.app.menu.toggle('Control Mouse (Zoom)', True) doesn't work and I don't know how to either provide or get the parent 'Eye Tracking'.
+    print("Calibrating eye tracking.")
+    eye_mouse.calib_start()
 
 def disable_eye_tracking():
     # Menu point stays unticked, don't know how to tick it.
@@ -17,6 +24,9 @@ def enable_eye_tracking():
 mod = Module()
 @mod.action_class
 class Actions:
+    def eyes_calib():
+        """Calibrates eye tracking."""
+        calibrate_eye_tracking()
     def eyes_on():
         """Enables eye tracking (zoom mouse)."""
         enable_eye_tracking()
--- eyetracking.talon
+++ eyetracking.talon
@@ -1,3 +1,4 @@
 # Only e.g. "eyes on", "tracking on", "eye tracking zoom", "track my eyes" cause false positives.
 track my eyes zoom: user.eyes_on()
 stop tracking my eyes: user.eyes_off()
+calibrate eye tracking: user.eyes_calib()
